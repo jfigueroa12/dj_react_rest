606424b8b1a24918d7f60e64aa7213119141f761
flyasth3sky <jonathan.figueroa@zoho.com>
2019-11-03 15:54:54 -0500

    Backend dev setup complete

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd /app/backend/
-until python3 manage.py migrate; do
-  sleep .1
-  echo "Retry!";
-done
-
+#until python3 manage.py migrate; do
+#  sleep
+#  echo "Retry!";
+#done
+sleep 1
 echo "Django is ready!";
 python3 manage.py runserver 0.0.0.0:8000
